60edaa5caa0704313972e3025000477973ea4e55
Oskar Hladky <oskar.hladky@bitpanda.com>
2018-01-23 14:57:49 +0100

    altcoin contants filled

--- btcpy/constants.py
+++ btcpy/constants.py
@@ -37,6 +37,67 @@ class BitcoinConstants(Constants):
     private_key_version_strings = {'mainnet': b'\x04\x88\xad\xe4', 'testnet': b'\x04\x35\x83\x94'}
 
 
+class DashConstants(Constants):
+
+    wif_prefixes = {'dash': 0xCC, 'dashtest': 0xEF}
+
+    raw_prefixes = {('dash', 'p2pkh'): bytearray(b'\x4c'),
+                    ('dashtest', 'p2pkh'): bytearray(b'\x8c'),
+                    ('dash', 'p2sh'): bytearray(b'\x13'),
+                    ('dashtest', 'p2sh'): bytearray(b'\x10')}
+
+    prefixes = {'X': ('p2pkh', 'dash'),
+                'y': ('p2pkh', 'dashtest'),
+                '8': ('p2pkh', 'dashtest'),
+                '7': ('p2sh', 'dash'),
+                '9': ('p2sh', 'dashtest')}
+
+    net_to_hrp = {'dash': 'bc',
+                  'dashtest': 'tb'}
+
+    hrp_to_net = {'bc': 'dash',
+                  'tb': 'dashtest'}
+
+    key_prefixes = {'x': 'dash', 't': 'dashtest'}
+
+    public_key_version_strings = {'dash': b'\x04\x88\xb2\x1e', 'dashtest': b'\x04\x35\x87\xcf'}
+
+    private_key_version_strings = {'dash': b'\x04\x88\xad\xe4', 'dashtest': b'\x04\x35\x83\x94'}
+
+
+class LitecoinConstants(Constants):
+
+    wif_prefixes = {'litecoin': 0xB0, 'litecointest': 0xEF}
+
+    raw_prefixes = {('litecoin', 'p2pkh'): bytearray(b'\x30'),
+                    ('litecointest', 'p2pkh'): bytearray(b'\x6f'),
+                    ('litecoin', 'p2sh'): bytearray(b'\x32'),
+                    ('litecointest', 'p2sh'): bytearray(b'\xc4')}
+
+    prefixes = {'L': ('p2pkh', 'litecoin'),
+                'M': ('p2sh', 'litecoin'),
+                'm': ('p2pkh', 'litecointest'),
+                'n': ('p2pkh', 'litecointest'),
+                '2': ('p2sh', 'litecointest'),
+                }
+
+    net_to_hrp = {'litecoin': 'bc',
+                  'litecointest': 'tb'}
+
+    hrp_to_net = {'bc': 'litecoin',
+                  'tb': 'litecointest'}
+
+    key_prefixes = {'x': 'litecoin', 't': 'litecointest'}
+
+    public_key_version_strings = {'litecoin': b'\x04\x88\xb2\x1e', 'litecointest': b'\x04\x35\x87\xcf'}
+
+    private_key_version_strings = {'litecoin': b'\x04\x88\xad\xe4', 'litecointest': b'\x04\x35\x83\x94'}
+
+
 NETWORKS = {'mainnet': BitcoinConstants(),
             'testnet': BitcoinConstants(),
-            'regtest': BitcoinConstants()}
+            'regtest': BitcoinConstants(),
+            'litecoin': LitecoinConstants(),
+            'litecointest': LitecoinConstants(),
+            'dash': DashConstants(),
+            'dashtest': DashConstants()}
